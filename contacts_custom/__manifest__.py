70caf326af34b5aea7ae57d2ca6cb1908575c163
3lnumag <3lnumag@gmail.com>
2021-05-20 18:01:57 -0500

    [ADD] Traducción contacto

---
+++ contacts_custom/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom contacts",
+
+    'summary': """Traducción de campos contactos""",
+
+    'description': """
+    """,
+
+    'author': "Wissen",
+    'website': "http://www.yourcompany.com",
+
+
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+
+    'depends': ['contacts'],
+
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+
+    'auto_install': True,
+}
