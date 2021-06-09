49addf8619fe85fc276cb6d0d144d4d50e705172
3lnumag <3lnumag@gmail.com>
2021-06-09 17:59:48 -0500

    [ADD] Traduccion a cotizacion en ventas

---
+++ sale_custom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom sale",
+
+    'summary': """Traducción de campo presupuesto a cotizacion""",
+
+    'description': """
+    """,
+
+    'author': "Wissen",
+    'website': "http://www.yourcompany.com",
+
+
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+
+    'depends': ['sale', 'sale_management', 'sale_stock'],
+
+    'data': [
+    ],
+
+    'auto_install': True,
+}
