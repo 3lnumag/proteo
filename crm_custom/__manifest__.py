6d618ad30e692d056f27d933287b172f83510e4f
3lnumag <3lnumag@gmail.com>
2021-06-09 19:46:12 -0500

    [ADD] crm trad cotización

---
+++ crm_custom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom crm",
+
+    'summary': """Traducción de campo presupuesto a cotizacion""",
+
+    'description': """
+    """,
+
+    'author': "Wissen",
+    'website': "http://www.yourcompany.com",
+
+
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+
+    'depends': ['sale', 'sale_crm'],
+
+    'data': [
+    ],
+
+    'auto_install': True,
+}
