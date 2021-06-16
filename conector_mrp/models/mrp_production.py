28df93a1bd14d05f16f0ad2f440c834711752e68
3lnumag <3lnumag@gmail.com>
2021-06-16 18:33:41 -0500

    [ADD] asys campos

--- conector_mrp/models/mrp_production.py
+++ conector_mrp/models/mrp_production.py
@@ -9,49 +9,52 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class MrpProduction(models.Model):
-   _inherit = 'mrp.production'
+    _inherit = 'mrp.production'
 
-   asys = fields.Boolean('ASYS', default=False, copy=False)
+    asys = fields.Boolean('ASYS', default=False, copy=False)
 
-   def enviar(self):
+    asys_state = fields.Selection([('send', 'Recibido'), ('error', 'No válido')], 'Estado Asys', default=None, readonly=True)
+    asys_error = fields.Text('Error Asys', readonly=True)
 
-      url = self.env.ref('conector_mrp.url_mrp').sudo().value
-      api_key = self.env.ref('conector_mrp.api_key_mrp').sudo().value
+    def enviar(self):
 
-      headers = {
-         'x-api-key': api_key,
-         'content-type': 'application/json'
-      }
-      data = self._data()
-      _logger.info(data)
+        url = self.env.ref('conector_mrp.url_mrp').sudo().value
+        api_key = self.env.ref('conector_mrp.api_key_mrp').sudo().value
 
-      response = requests.post(url, data=json.dumps(data), headers=headers)
-      _logger.info(response)
+        headers = {
+            'x-api-key': api_key,
+            'content-type': 'application/json'
+        }
+        data = self._data()
+        _logger.info(data)
 
-      if response.status_code == requests.codes.ok:
-         pass
-      else:
-         raise UserError("Error de conexión:\n%s" % response)
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        _logger.info(response)
 
-   def _data(self):
-      dic = {}
-      components = []
+        if response.status_code == requests.codes.ok:
+            pass
+        else:
+            raise UserError("Error de conexión:\n%s" % response)
 
-      dic['id_op'] = self.id
-      dic['no_op'] = self.name
-      dic['product_produce'] = self.product_id.name
-      dic['product_code_produce'] = self.product_id.default_code
-      dic['quantity_produce'] = self.product_qty
+    def _data(self):
+        dic = {}
+        components = []
 
+        dic['id_op'] = self.id
+        dic['no_op'] = self.name
+        dic['product_produce'] = self.product_id.name
+        dic['product_code_produce'] = self.product_id.default_code
+        dic['quantity_produce'] = self.product_qty
 
-      for l in self.move_raw_ids:
-         dic2 = {}
-         dic2['product_name'] = l.product_id.name
-         dic2['product_code'] = l.product_id.default_code
-         dic2['target_weight'] = l.product_uom_qty
-         components.append(dic2)
+        for l in self.move_raw_ids:
+            dic2 = {}
+            dic2['product_name'] = l.product_id.name
+            dic2['product_code'] = l.product_id.default_code
+            dic2['target_weight'] = l.product_uom_qty
+            components.append(dic2)
 
-      dic['components'] = components
+        dic['components'] = components
 
-      return dic
+        return dic
