2be62ec4d0ab023c37b126a4b78c7dd84771e122
3lnumag <3lnumag@gmail.com>
2021-06-22 21:51:10 -0500

    [ADD] trad cotizacion en compras

---
+++ purchase_custom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom purchase",
+
+    'summary': """Traducción de campo presupuesto a cotizacion""",
+
+    'description': """
+    """,
+
+    'author': "Wissen",
+    'website': "http://www.yourcompany.com",
+
+
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+
+    'depends': ['purchase'],
+
+    'data': [
+    ],
+
+    'auto_install': True,
+}
