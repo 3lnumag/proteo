982a98fb5ffe518c2d68512d26b5c8e25f2451c4
3lnumag <3lnumag@gmail.com>
2021-06-28 18:38:14 -0500

    [ADD] ac_report (apuntes contables)

---
+++ ac_reporting/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+   'name': "AC Reporting",
+
+   'summary': """AC Reporting""",
+
+   'description': """
+        
+    """,
+
+   'author': "Wissen",
+   'website': "http://www.yourcompany.com",
+
+   'category': 'Uncategorized',
+   'version': '14.0.1',
+
+   'depends': ['account'],
+
+   'data': [
+      'report/ac_report.xml',
+      'report/ac_report_template.xml',
+   ],
+   'installable': True,
+   'application': False,
+   'auto_install': False,
+}
